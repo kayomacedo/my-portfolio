4265b3ef2bbfad5af5cfcd0f1f22fae3e9a21063
Kayo Macedo <57056101+kayomacedo@users.noreply.github.com>
2024-01-16 03:07:56 -0800

    coloquei os links

--- app/components/Projects/projects.js
+++ app/components/Projects/projects.js
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import SectionTitle from "../sectionTitle/section-title";
 import './projects.scss';
-import { Navigation, Pagination, Autoplay,EffectCube } from 'swiper/modules';
-import { useState,useEffect,useRef } from "react";
+import { Navigation, Pagination, Autoplay, EffectCube } from 'swiper/modules';
+import { useState, useEffect, useRef } from "react";
 import { VscGithubAlt } from "react-icons/vsc";
 
 
@@ -11,69 +11,73 @@ import { VscGithubAlt } from "react-icons/vsc";
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Projects() {
-    const [slidePerview,setSlidePerview] = useState(2);
-    const data = [
-        { id: '1', img: '/project1.gif',url:'https://github.com/kayomacedo/gerenciador-de-contas-app' },
-        { id: '2', img: '/project1.gif', url:'https://github.com/kayomacedo/gerenciador-de-contas-app' }
-     
-       
-
-    ]
-
-    useEffect(()=>{
-        function handleResize(){
-            setSlidePerview(window.innerWidth > 768? 2 : 1);
-
-        }
-        handleResize();
-        window.addEventListener('resize',handleResize);
-        return ()=>{
-            window.removeEventListener('resize',handleResize);
-        }
-
-    },[]);
-    return (
-        <div className="projects">
-            <div className="title">
-                
-            <SectionTitle
-                text={'Projects'}
-               
-            />
-            
-            </div>
-            <div className="gallery">
-            <Swiper
-        effect={'cube'}
-        grabCursor={true}
-        cubeEffect={{
-          shadow: true,
-          slideShadows: true,
-          shadowOffset: 20,
-          shadowScale: 0.94,
-        }}
-        pagination={true}
-        modules={[EffectCube, Pagination]}
-        className="mySwiper sliderItem"
-      >
-        <SwiperSlide>
-          <Image src={'/appGerenciador.png'} height={300} width={300} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={'/appGerenciador2.png'} height={300} width={300} />
-        
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <Image src={'/appLight.png'} height={300} width={300} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={'/appLight2.png'} height={300} width={300} />
-        </SwiperSlide>
-      </Swiper>
+  const [slidePerview, setSlidePerview] = useState(2);
+  const data = [
+    { id: '1', img: '/project1.gif', url: 'https://github.com/kayomacedo/gerenciador-de-contas-app' },
+    { id: '2', img: '/project1.gif', url: 'https://github.com/kayomacedo/gerenciador-de-contas-app' }
 
 
-            </div>
+
+  ]
+
+  useEffect(() => {
+    function handleResize() {
+      setSlidePerview(window.innerWidth > 768 ? 2 : 1);
+
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+
+  }, []);
+  return (
+    <div className="projects">
+      <div className="title">
+
+        <SectionTitle
+          text={'Projects'}
+
+        />
+
+      </div>
+      <div className="gallery">
+        <Swiper
+          effect={'cube'}
+          grabCursor={true}
+          cubeEffect={{
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+          }}
+          pagination={true}
+          modules={[EffectCube, Pagination]}
+          className="mySwiper sliderItem"
+        >
+          <SwiperSlide>
+            <Image src={'/appGerenciador.png'} height={300} width={300} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={'/appGerenciador2.png'} height={300} width={300} />
+
+          </SwiperSlide>
+          <SwiperSlide>
+
+            <Image src={'/appLight.png'} height={300} width={300} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={'/appLight2.png'} height={300} width={300} />
+          </SwiperSlide>
+        </Swiper>
+        <div className="button">
+
+        <a href="https://github.com/kayomacedo/gerenciador-de-contas-app" target="_blank" className="textButon">On Github</a>
         </div>
-    )
+
+
+      </div>
+    </div>
+  )
 }
--- app/components/Social/social.js
+++ app/components/Social/social.js
@@ -7,19 +7,19 @@ import './social.scss';
 export default function MenuSocial() {
     return (
         <div className="menu-social">
-            <a href="#">
+            <a href="https://www.instagram.com/kayomacedo/" target="_blank">
 
                 <FaInstagram className="icon" />
             </a>
-            <a href="#">
+            <a href="https://www.threads.net/@kayomacedo" target="_blank">
 
                 <FaThreads className="icon" />
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/kayomacedo/" target="_blank">
 
                 <CiLinkedin className="icon" />
             </a>
-            <a href="#">
+            <a href="https://github.com/kayomacedo/" target="_blank">
 
                 <VscGithubAlt className="icon" />
             </a>
